d7c66418a3a22588decd8faafca13bed72ee0421
David Roman <2538074+davdroman@users.noreply.github.com>
2025-10-19 22:56:07 +0200

    Bump Swift Syntax (#3)

--- Package.swift
+++ Package.swift
@@ -6,11 +6,11 @@ import CompilerPluginSupport
 let package = Package(
 	name: "swift-once-macro",
 	platforms: [
-		.macOS(.v10_15),
 		.iOS(.v13),
+		.macOS(.v10_15),
 		.tvOS(.v13),
-		.watchOS(.v6),
 		.visionOS(.v1),
+		.watchOS(.v6),
 	],
 	products: [
 		.library(name: "Once", targets: ["Once"]),
@@ -38,13 +38,12 @@ let package = Package(
 
 package.dependencies += [
 	.package(url: "https://github.com/pointfreeco/swift-macro-testing", from: "0.6.0"),
-	.package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"602.0.0"),
+	.package(url: "https://github.com/swiftlang/swift-syntax", "600.0.0"..<"603.0.0"),
 ]
 
 for target in package.targets {
 	target.swiftSettings = target.swiftSettings ?? []
 	target.swiftSettings? += [
-		.swiftLanguageMode(.v6),
 		.enableUpcomingFeature("ExistentialAny"),
 		.enableUpcomingFeature("InternalImportsByDefault"),
 	]
